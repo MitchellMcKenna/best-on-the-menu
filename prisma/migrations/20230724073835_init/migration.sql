04fa66bad3e38e7d2b575afcfd3c1030e03435b5
Mitchell McKenna <mitchellmckenna@gmail.com>
2023-07-29 22:13:10 -0700

    feat(db): business table migration
    - Update initial migration to include the Business table schema.

--- prisma/migrations/20230724073835_init/migration.sql
+++ prisma/migrations/20230724073835_init/migration.sql
@@ -1,3 +1,7 @@
+-- CreateExtension
+CREATE SCHEMA IF NOT EXISTS "geo";
+CREATE EXTENSION IF NOT EXISTS "postgis" WITH SCHEMA "geo";
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
@@ -21,8 +25,32 @@ CREATE TABLE "Post" (
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Business" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" geometry(Point, 4326) NOT NULL,
+    "amenity" TEXT,
+    "cuisine" TEXT,
+    "website" TEXT,
+    "streetNumber" TEXT,
+    "street" TEXT,
+    "city" TEXT,
+    "state" TEXT,
+    "postcode" TEXT,
+    "country" TEXT,
+    "hours" TEXT,
+
+    CONSTRAINT "Business_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "location_idx" ON "Business" USING GIST ("location");
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
