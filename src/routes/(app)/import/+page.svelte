68df763a83344751c5a8ab1c454c695cde002b0f
Mitchell McKenna <mitchellmckenna@gmail.com>
2026-02-16 22:58:07 -0800

    wip

---
+++ src/routes/(app)/import/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+	$: ({ user } = data);
+
+</script>
+
+<div class="grid h-screen place-items-center">
+    <button class="btn btn-primary">import restaurants.pbf</button>
+</div>
